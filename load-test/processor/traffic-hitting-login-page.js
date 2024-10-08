f6ec7343f696f0860792e5a7e079fa19cfbc2e95
juanq4 <juan.mejia@q4inc.com>
2024-10-08 11:28:18 -0400

    wip

---
+++ load-test/processor/traffic-hitting-login-page.js
@@ -0,0 +1,47 @@
+function getInteractionId(store) {
+  const interactionPath = Object.keys(store).find(path =>
+    path.startsWith("/interaction/")
+  );
+
+  if (!interactionPath) return null;
+
+  const interactionId = interactionPath.split("/interaction/")[1];
+  return interactionId;
+}
+
+function getCookies(cookieObj) {
+  return Object.keys(cookieObj)
+    .map(key => {
+      const match = `${cookieObj[key]}`.match(/=(.*?);/);
+      return match ? `${key}=${match[1].trim()}` : "";
+    })
+    .join("; ");
+}
+
+exports.beforeLogin = (req, context, _events, next) => {
+  const store = context._jar._jar.store.idx["stage.identity.q4inc.com"];
+
+  const interactionId = getInteractionId(store);
+
+  if (!interactionId) {
+    console.error("Interaction id not found");
+    return next();
+  }
+
+  console.log("Interaction Id", interactionId);
+
+  const cookieObj = store[`/interaction/${interactionId}`];
+  const cookies = getCookies(cookieObj);
+
+  if (!cookies) {
+    console.error("Cookies not found");
+    return next();
+  }
+
+  console.log("cookies", cookies);
+
+  req.headers.Cookie = cookies;
+  req.url = `https://stage.identity.q4inc.com/interaction/${interactionId}/login`;
+
+  return next();
+};
